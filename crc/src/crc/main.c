ba9b9a790b2e13b33be96cc32f82c1461244f669
Patrick Flynn <pflynn157@gmail.com>
2025-07-09 15:43:48 -0400

    CRC change

--- crc/src/crc/main.c
+++ crc/src/crc/main.c
@@ -12,36 +12,48 @@
  * notice must not be changed or removed and no warranty is either
  * expressed or implied by its publication or distribution.
  **********************************************************************/
-#include "printf.h"
+
 #include <stdio.h>
 #include <string.h>
 
+#include "common.h"
+#include "../text_input_16k.h"
 #include "crc.h"
 
-
-int
-main(void)
+void crc_test(void)
 {
-	unsigned char  test[] = "123456789";
+    uint8_t test[] = "123456789";
+
+    /*
+     * Print the check value for the selected CRC algorithm.
+     */
+    printf("The check value for the %s standard is 0x%X\n", CRC_NAME, CHECK_VALUE);
+
+    /*
+     * Compute the CRC of the test message, slowly.
+     */
+    printf("The crcSlow() of \"123456789\" is 0x%X\n", crcSlow(test, strlen((const char*)test)));
+    crcSlow(test, 9);
+
+    /*
+     * Compute the CRC of the test message, more efficiently.
+     */
+    crcInit();
+    printf("The crcFast() of \"123456789\" is 0x%X\n", crcFast(test, strlen((const char*)test)));
+    crcFast(test, 9);
+}
 
 
-	/*
-	 * Print the check value for the selected CRC algorithm.
-	 */
-	printf("The check value for the %s standard is 0x%X\n", CRC_NAME, CHECK_VALUE);
-	
-	/*
-	 * Compute the CRC of the test message, slowly.
-	 */
-	printf("The crcSlow() of \"123456789\" is 0x%X\n", crcSlow(test, strlen(test)));
-  crcSlow(test,9);
-	
-	/*
-	 * Compute the CRC of the test message, more efficiently.
-	 */
-	crcInit();
-	printf("The crcFast() of \"123456789\" is 0x%X\n", crcFast(test, strlen(test)));
-  crcFast(test,9);
+int main(void)
+{
+    crcInit();
+    volatile uint32_t noprint_output;
 
-  return 0;
-}   /* main() */
+    printf("Computing the CRC of a %zu byte message\r\n", strlen((const char*)test_data));
+    uint32_t crc = crcFast(test_data, strlen((const char*)test_data));
+    printf("%08X\r\n", crc);
+    noprint_output = crc;
+    (void)noprint_output;
+
+    return 0;
+}
