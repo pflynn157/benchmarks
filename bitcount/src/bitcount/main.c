14d960dd62fa6eb915b1a91967078ec8126e259a
Patrick Flynn <pflynn157@gmail.com>
2025-07-09 20:29:44 -0400

    bitcount

--- bitcount/src/bitcount/main.c
+++ bitcount/src/bitcount/main.c
@@ -14,8 +14,18 @@
 #define RNG_SEED   0x0C728394
 #define ITERATIONS 100000
 
-void my_srand(uint32_t new_seed);
-uint32_t my_rand(void);
+static uint32_t _myrand_seed = 1234;
+
+void my_srand(uint32_t new_seed)
+{
+    _myrand_seed = new_seed;
+}
+
+uint32_t my_rand(void)
+{
+    _myrand_seed = (uint32_t)(1103515245 * _myrand_seed + 12345);
+    return _myrand_seed;
+}
 
 static uint32_t bit_shifter(uint32_t x);
 
