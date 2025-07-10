0ba143c252a1ff42b2fd85c74016df45f6b22994
Patrick Flynn <pflynn157@gmail.com>
2025-07-10 16:39:19 -0400

    updates

--- make-benchmarks.mk
+++ make-benchmarks.mk
@@ -30,7 +30,7 @@
 # susan_edges
 # susan_smooth
 BENCHMARKS := \
-		anomaly
+		fft
 
 # Get the different configuration targets from benchmark-build
 CONFIGURATIONS := $(shell benchmark-build targets)
