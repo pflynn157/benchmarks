4cbdeaf8dd8d3faa32642a31821b7d302bedfca0
Patrick Flynn <pflynn157@gmail.com>
2025-07-10 15:12:52 -0400

    Removed old code

--- OLD/sha/src/portme/fread.h
+++
@@ -1,4 +0,0 @@
-
-#include <stdlib.h>
-size_t fread_(void *ptr, size_t size, size_t nmemb, char *stream);
-
--- OLD/sha/src/sha/test.asc.h
+++
@@ -1,5 +0,0 @@
-char inputString[] = 
-"one line\n"
-"two line\n"
-"3 line\n"
-"sdada\n";
