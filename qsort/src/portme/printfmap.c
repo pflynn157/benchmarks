e91f17c02c202699f55974bb47476b54821b149b
Patrick Flynn <pflynn157@gmail.com>
2026-01-27 11:52:59 -0500

    New printf map

--- qsort/src/portme/printfmap.c
+++ qsort/src/portme/printfmap.c
@@ -1,4 +1,5 @@
 #include <stddef.h>
+#include <stdint.h>
 
 #if defined(__clang__)
 #define NO_OPT __attribute__((optnone))
@@ -9,8 +10,17 @@
 NO_OPT
 int putc(int character, void *stream)
 {
-    (void)stream;
-    return character;
+    //(void)stream;
+    //return character;
+    //#ifdef ARM
+    uint8_t *addr = (uint8_t*)0xE0000000;
+    //#elif BMSP430
+    //uint8_t *addr = (uint8_t*)0xFFF2;
+    //#else
+    //uint8_t *addr = (uint8_t*)0x40000000;
+    //#endif
+    *addr = (char)character;
+    return 0;
 }
 
 NO_OPT
--- susan_smooth/src/portme/printfmap (copy).c
+++ susan_smooth/src/portme/printfmap (copy).c
@@ -1,5 +1,4 @@
 #include <stddef.h>
-#include <stdint.h>
 
 #if defined(__clang__)
 #define NO_OPT __attribute__((optnone))
@@ -10,17 +9,8 @@
 NO_OPT
 int putc(int character, void *stream)
 {
-    //(void)stream;
-    //return character;
-    //#ifdef ARM
-    uint8_t *addr = (uint8_t*)0xE0000000;
-    //#elif BMSP430
-    //uint8_t *addr = (uint8_t*)0xFFF2;
-    //#else
-    //uint8_t *addr = (uint8_t*)0x40000000;
-    //#endif
-    *addr = (char)character;
-    return 0;
+    (void)stream;
+    return character;
 }
 
 NO_OPT
