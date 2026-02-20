812380cc8d6319b4a3a1d56ed3f2ca843d9191b6
Patrick Flynn <pflynn157@gmail.com>
2026-02-20 13:54:52 -0500

    Fix

--- lorawan_down/src/lorawan_down/lorawan_down_test.c
+++ lorawan_down/src/lorawan_down/lorawan_down_test.c
@@ -1,8 +1,8 @@
-#include "printf.h"
 #include <stdint.h>
 #include <string.h>
 #include <stdio.h>
 
+#include "common.h"
 #include "lw.h"
 #include "inputs.h"
 
@@ -15,15 +15,18 @@ void print_hex_array(const char *preamble, const uint8_t *array, const int len)
         printf("%s", preamble);
     }
 
-    printf("0x");
     for (int i = 0; i < len; i++)
     {
-        printf("%02X", array[i]);
+        printf("%02X ", array[i]);
+        if ((i + 1) % 32 == 0)
+        {
+            printf("\r\n");
+        }
     }
     printf("\r\n");
 }
 
-int main()
+int benchmark_main()
 {
     lw_frame_t frame;
     lw_key_grp_t kgrp;
@@ -53,9 +56,9 @@ int main()
     }
     if (ret == LW_OK)
     {
-        printf("App nonce: 0x%06X\r\n", frame.pl.ja.appnonce.data);
-        printf("Net ID: 0x%06X\r\n", frame.pl.ja.netid.data);
-        printf("Dev Address: 0x%08X\r\n", frame.pl.ja.devaddr.data);
+        printf("App nonce: 0x%06lX\r\n", frame.pl.ja.appnonce.data);
+        printf("Net ID: 0x%06lX\r\n", frame.pl.ja.netid.data);
+        printf("Dev Address: 0x%08lX\r\n", frame.pl.ja.devaddr.data);
         printf("RX2DataRate: %u\r\n", frame.pl.ja.dlsettings.bits.rx2dr);
         printf("RX1DRoffset: %u\r\n", frame.pl.ja.dlsettings.bits.rx1droft);
         print_hex_array("App Session Key: ", frame.pl.ja.appskey, 16);
@@ -86,7 +89,7 @@ int main()
     }
     if (ret == LW_OK)
     {
-        printf("Dev Address: 0x%08X\r\n", frame.pl.mac.devaddr.data);
+        printf("Dev Address: 0x%08lX\r\n", frame.pl.mac.devaddr.data);
         printf("FCnt: %u\r\n", frame.pl.mac.fcnt);
         printf("Port: %u\r\n", frame.pl.mac.fport);
         printf("ACK: %u\r\n", frame.pl.mac.fctrl.ul.ack);
